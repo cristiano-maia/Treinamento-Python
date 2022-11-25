e3f406a75a3da65e990ffe1b3334c2447b0c0838
Cristiano Maia <cristianoaj@gmail.com>
2022-11-25 14:50:10 -0300

    Atualização

---
+++ exercicios_python/ex013.py
@@ -0,0 +1,7 @@
+"""
+Faça uma lista de compras com listas
+O usuario deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com indices inexistentes da lista
+"""
+
